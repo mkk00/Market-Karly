4561c0e72091ff58631e046fe890fb050dbc6207
ukssss <1872003115su@gmail.com>
2023-01-29 11:55:27 +0900

    ESLint Rules 설정 및 Prettier 연결

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,62 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    'env': {
+      'browser': true,
+      'es2021': true,
+      'node': true
     },
-    "extends": "eslint:recommended",
-    "overrides": [
+    'extends': ['eslint:recommended', 'prettier'],
+    'overrides': [
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    'parserOptions': {
+      'ecmaVersion': 'latest',
+      'sourceType': 'module'
     },
-    "rules": {
+    'rules': {
+      'no-unused-vars':'warn',
+      'no-unused-expressions': ['error'],
+      'no-unreachable-loop': ['error'],
+      'no-unreachable': ['error'],
+      'no-param-reassign': ['error'],
+      'no-use-before-define': ['error'],
+      'max-depth': [
+      'error',
+      {
+        max: 1,
+      },
+      ],
+      'padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: 'return',
+        },
+      ],
+      'no-unneeded-ternary': [
+        'error',
+        {
+          defaultAssignment: false,
+        },
+      ],
+      quotes: [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'brace-style': ['error'],
+      curly: ['error', 'multi-line', 'consistent'],
+      'no-debugger': ['warn'],
+      'no-restricted-globals': ['off'],
+      'no-alert': ['warn'],
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
     }
-}
+  }
