91a0ff3b93c35f511405072f31e4959052659250
tramin <mk1210k@gmail.com>
2023-02-02 23:24:53 +0900

    [feat] js 파일 연결 및 import 파일 경로 수정

---
+++ client/js/main/mkk_swiper.js
@@ -0,0 +1,63 @@
+// {#ddd} main banner - swiper
+
+var swiper = new Swiper(".banner-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {     //자동슬라이드 (false-비활성화)
+    delay: 4000, // 시간 설정
+    disableOnInteraction: false, // false-스와이프 후 자동 재생
+  },
+  keyboard: {
+    // enabled: true,
+  },
+  loopAdditionalSlides : 1,
+    // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".banner-swiper .swiper-button-next",
+    prevEl: ".banner-swiper .swiper-button-prev",
+  },
+});
+
+
+
+// {#ddd} today, recommend product 
+
+var swiper = new Swiper(".today-product-swiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  keyboard: {
+    // enabled: true,
+  },
+  loopAdditionalSlides : 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".today-products .swiper-button-next",
+    prevEl: ".today-products .swiper-button-prev",
+  },
+});
+
+
+var swiper = new Swiper(".recommend-product-swiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  keyboard: {
+    // enabled: false,
+  },
+  loopAdditionalSlides : 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".recommend-products .swiper-button-next",
+    prevEl: ".recommend-products .swiper-button-prev",
+  },
+});
