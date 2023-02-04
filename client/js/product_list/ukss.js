3eebbfb56ee3df831c5834bc85d75c42cb9309af
ukssss <1872003115su@gmail.com>
2023-02-04 23:17:07 +0900

    [fix] Product-List | 초기화 버튼 누르면 체크상태 수량이 초기화되지 않는 문제 해결 #150
    - 초기화 버튼 누르면 체크상태 수량이 초기화되지 않는 문제 해결

--- client/js/product_list/ukss.js
+++ client/js/product_list/ukss.js
@@ -7,7 +7,6 @@
 const checkBtn = document.querySelectorAll('.accordion_item__inputCheck');
 const moreBtn = document.querySelectorAll('.accordion_head__btn');
 const resetBtn = document.querySelector('.frame70__btn');
-const status = document.querySelectorAll('.frame70__status');
 
 // 체크박스
 function handleCheckbox(e) {
@@ -29,27 +28,29 @@ function handleMore(e) {
 function handleReset(e) {
   e.preventDefault();
   let active = document.querySelectorAll('.accordion_set .is-active');
+  let status = document.querySelectorAll('.frame70__status');
   let activeArr = Array.from(active);
 
   activeArr.forEach((active) => active.classList.remove('is-active'));
+  status.forEach((stat) => (stat.innerText = ''));
 }
 
 // 상태 변화
 export function handleStatus(e) {
   e.preventDefault();
 
-  let list = e.target.closest('.accordion_set');
-  let active = list.querySelectorAll('.is-active');
-  let num = list.querySelector('.frame70__status');
+  let checkList = e.target.closest('.accordion_set');
+  let active = checkList.querySelectorAll('.is-active');
+  let status = checkList.querySelector('.frame70__status');
 
   if (!active.length) {
-    num.innerText = '';
+    status.innerText = '';
   } else {
-    num.innerText = active.length;
+    status.innerText = active.length;
   }
 }
 
 checkBtn.forEach((check) => check.addEventListener('click', handleCheckbox));
 checkBtn.forEach((check) => check.addEventListener('click', handleStatus));
-moreBtn.forEach((set) => set.addEventListener('click', handleMore));
+moreBtn.forEach((more) => more.addEventListener('click', handleMore));
 resetBtn.addEventListener('click', handleReset);
