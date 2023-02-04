140b4f09ec982dd92b510d727268354776456e8c
ukssss <1872003115su@gmail.com>
2023-02-04 22:36:08 +0900

    Product-List | Navigator 체크항목 수량 표시 구현 #148
    - Navigator 체크항목 추가하면 실시간으로 수량 변화가 표시가 됨

--- client/js/product_list/ukss.js
+++ client/js/product_list/ukss.js
@@ -7,7 +7,9 @@
 const checkBtn = document.querySelectorAll('.accordion_item__inputCheck');
 const moreBtn = document.querySelectorAll('.accordion_head__btn');
 const resetBtn = document.querySelector('.frame70__btn');
+const status = document.querySelectorAll('.frame70__status');
 
+// 체크박스
 function handleCheckbox(e) {
   e.preventDefault();
   let select = e.target.closest('.accordion_item__checkbox');
@@ -15,6 +17,7 @@ function handleCheckbox(e) {
   select.classList.toggle('is-active');
 }
 
+// 더보기
 function handleMore(e) {
   e.preventDefault();
   let more = e.target.closest('.accordion_set');
@@ -22,6 +25,7 @@ function handleMore(e) {
   more.classList.toggle('is-active');
 }
 
+// 초기화
 function handleReset(e) {
   e.preventDefault();
   let active = document.querySelectorAll('.accordion_set .is-active');
@@ -30,6 +34,22 @@ function handleReset(e) {
   activeArr.forEach((active) => active.classList.remove('is-active'));
 }
 
+// 상태 변화
+export function handleStatus(e) {
+  e.preventDefault();
+
+  let list = e.target.closest('.accordion_set');
+  let active = list.querySelectorAll('.is-active');
+  let num = list.querySelector('.frame70__status');
+
+  if (!active.length) {
+    num.innerText = '';
+  } else {
+    num.innerText = active.length;
+  }
+}
+
 checkBtn.forEach((check) => check.addEventListener('click', handleCheckbox));
+checkBtn.forEach((check) => check.addEventListener('click', handleStatus));
 moreBtn.forEach((set) => set.addEventListener('click', handleMore));
 resetBtn.addEventListener('click', handleReset);
