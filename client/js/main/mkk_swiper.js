298e7a7884a56c4ecd9594f94bd44cd5cdacd721
tramin <mk1210k@gmail.com>
2023-02-06 23:27:03 +0900

    [fix] Main Banner Arrow 고정 #107
    브라우저 확대시 UI 무너짐 개선

--- client/js/main/mkk_swiper.js
+++ client/js/main/mkk_swiper.js
@@ -2,14 +2,11 @@
 
 var swiper = new Swiper(".banner-swiper", {
   slidesPerView: 1,
-  spaceBetween: 30,
   loop: true,
+  speed: 1300,
   autoplay: {     //자동슬라이드 (false-비활성화)
     delay: 4000, // 시간 설정
     disableOnInteraction: false, // false-스와이프 후 자동 재생
-  },
-  keyboard: {
-    // enabled: true,
   },
   loopAdditionalSlides : 1,
     // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정 
@@ -30,9 +27,6 @@ var swiper = new Swiper(".banner-swiper", {
 var swiper = new Swiper(".today-product-swiper", {
   slidesPerView: 4,
   spaceBetween: 30,
-  keyboard: {
-    // enabled: true,
-  },
   loopAdditionalSlides : 1,
   pagination: {
     el: ".swiper-pagination",
@@ -48,9 +42,6 @@ var swiper = new Swiper(".today-product-swiper", {
 var swiper = new Swiper(".recommend-product-swiper", {
   slidesPerView: 4,
   spaceBetween: 30,
-  keyboard: {
-    // enabled: false,
-  },
   loopAdditionalSlides : 1,
   pagination: {
     el: ".swiper-pagination",
