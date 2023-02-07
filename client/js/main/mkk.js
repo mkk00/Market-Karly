656c084569161c91026bd22c0d6054ae053cd6bc
mkk <mk1210k@gmail.com>
2023-02-07 11:05:50 +0900

    [feat] add cart 팝업창, css 수정 #64 #117
    - cart icon 클릭시 팝업창 보이기
    - +, - 버튼 조작 시 아이콘 색상 변경 및 수량, total price 변경

--- client/js/main/mkk.js
+++ client/js/main/mkk.js
@@ -1,9 +1,8 @@
-import { getNode } from '../../lib/index.js';
+import { getNode, getNodes, attr } from '../../lib/index.js';
 
 /* -------------------------------------------------------------------------- */
 /*                                    main                                    */
 /* -------------------------------------------------------------------------- */
-
 
 
 
@@ -32,7 +31,7 @@ $mainBanner.addEventListener('mouseleave', mainBannerArrowOut)
 
 
 
-// pop-up
+// {#ddd} pop-up
 
 const $popupClose = getNode('.popup__btn button:last-child');
 const $popupTodayClose = getNode('.popup__btn button:first-child');
@@ -46,3 +45,92 @@ function closePopup(){
 
 $popupClose.addEventListener('click', closePopup);
 $popupDim.addEventListener('qclick', closePopup);
+
+
+// {#ddd} add cart
+
+// add cart event
+const $cartIcon = getNodes('.today-products__add-cart img, .recommend-products__add-cart img');
+const $cartBox = getNode('.add-cart');
+
+for(const button of $cartIcon){
+  button.addEventListener('click', showCartBox);
+}
+
+function showCartBox(e){
+  e.preventDefault();
+  e.stopPropagation();
+  $cartBox.style.display = "block";
+  $popupDim.style.display = "block";
+}
+
+const $cartCancel = getNode('.add-cart__cancel');
+
+function closeCartBox(){
+  $cartBox.style.display = "none";
+  $popupDim.style.display = "none";
+}
+
+$cartCancel.addEventListener('click', closeCartBox);
+
+
+// add cart up&down icon event
+const $countPlus = getNode('.product-count__plus');
+const $countMinus = getNode('.product-count__minus');
+const $countNum = getNode('#product-count');
+const $countIconPlus = getNode('.product-count__plus img');
+const $countIconMinus = getNode('.product-count__minus img');
+
+// price 에서 숫자만 추출, 회계 단위로 표현하는 함수
+const $priceTotal = getNode('.add-cart__total-price');
+let $productPrice = getNode('.add-cart__price').textContent;
+let setCount = Number($countNum.value);
+
+let $parseIntPrice = parseInt($productPrice.replaceAll(/,/g, ""))
+
+let getOnlyPrice = function(node){
+  return parseInt(node.replaceAll(/,/g, ""));
+}
+
+let calculateTotalPrice = function (){
+  $priceTotal.textContent = (setCount * $parseIntPrice).toLocaleString() + '원'
+}
+
+
+function productCountUp(){
+  if(Number($countNum.value)>=99){
+    setCount = 99;
+    $countNum.value = setCount;
+    calculateTotalPrice();
+  } else if(Number($countNum.value)>=98){
+    attr($countIconPlus, 'src', '../assets/icons/Icon/count_plus_off.svg');
+    setCount++;
+    $countNum.value = setCount;
+    calculateTotalPrice();
+  } else{
+    setCount++;
+    $countNum.value = setCount;
+    calculateTotalPrice();
+  }
+  attr($countIconMinus, 'src', '../assets/icons/Icon/count_minus_on.svg');  
+}
+function productCountDown(){
+  if(Number($countNum.value)<=1){
+    setCount = 1;
+    $countNum.value = setCount;
+    calculateTotalPrice();
+  } else if(Number($countNum.value)<=2){
+    attr($countIconMinus, 'src', '../assets/icons/Icon/count_minus_off.svg');
+    setCount--;
+    $countNum.value = setCount;
+    calculateTotalPrice();
+  } else{
+    setCount--;
+    $countNum.value = setCount;
+    calculateTotalPrice();
+  }
+  attr($countIconPlus, 'src', '../assets/icons/Icon/count_plus_on.svg');
+}
+
+$countPlus.addEventListener('click', productCountUp);
+$countMinus.addEventListener('click', productCountDown);
