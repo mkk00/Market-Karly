58543d440bd65d97378dc0079c0bdcfed498e1f3
SJM <warning6715@gmail.com>
2023-02-08 17:45:21 +0900

    [feat] +-버튼, x버튼, 계산기 기능 추가 #143 #146 #133
    - npm start로 실행 시 체크박스 안뜨는 오류 고침
    - +-버튼 클릭시 '원'과 겹쳐서 보이는 현상 고침
    - x버튼 클릭 시 장바구니에서 사라짐
    - 계산기 기능 구현

--- client/js/cart/jm.js
+++ client/js/cart/jm.js
@@ -16,31 +16,42 @@ let ptn1=document.getElementById('pbtn-1');
 let ptn2=document.getElementById('pbtn-2');
 let mtn1=document.getElementById('mbtn-1');
 let mtn2=document.getElementById('mbtn-2');
+let smoney=document.getElementById('s-money');
+let fmoney=document.getElementById('f-money');
 
 ptn1.addEventListener('click', function(){
   answer1.innerHTML=(parseInt(answer1.innerHTML)+1);
   money1.innerHTML=(parseInt(money1.innerHTML)+4980)+'원';
+  smoney.innerHTML=(parseInt(smoney.innerHTML)+4980)+'원';
+  fmoney.innerHTML=(parseInt(fmoney.innerHTML)+4980);
 });
 mtn1.addEventListener('click',function(){
-  answer1.innerHTML=parseInt((answer1.innerHTML)-1);
-  money1.innerHTML=parseInt((money1.innerHTML)-4980);
+  answer1.innerHTML=(parseInt(answer1.innerHTML)-1);
+  money1.innerHTML=(parseInt(money1.innerHTML)-4980)+'원';
+  smoney.innerHTML=(parseInt(smoney.innerHTML)-4980)+'원';
+  fmoney.innerHTML=(parseInt(fmoney.innerHTML)-4980);
 })
 
 ptn2.addEventListener('click', function(){
   answer2.innerHTML=(parseInt(answer2.innerHTML)+1);
-  money2.innerHTML=(parseInt(money2.innerHTML)+4980);
+  money2.innerHTML=(parseInt(money2.innerHTML)+4980)+'원';
+  smoney.innerHTML=(parseInt(smoney.innerHTML)+4980)+'원';
+  fmoney.innerHTML=(parseInt(fmoney.innerHTML)+4980);
 });
 mtn2.addEventListener('click',function(){
-  answer2.innerHTML=parseInt((answer2.innerHTML)-1);
-  money2.innerHTML=parseInt((money2.innerHTML)-4980);
+  answer2.innerHTML=(parseInt(answer2.innerHTML)-1);
+  money2.innerHTML=(parseInt(money2.innerHTML)-4980)+'원';
+  smoney.innerHTML=(parseInt(smoney.innerHTML)-4980)+'원';
+  fmoney.innerHTML=(parseInt(fmoney.innerHTML)-4980);
 })
 
 let allchecker=document.getElementsByName('checker');
 let selectall=document.getElementById('checker1');
+let checker3=document.getElementById('checker3');
+let checker4=document.getElementById('checker4');
+
 selectall.addEventListener('change',function(){
-  
-    allchecker='checked';
-  
+  checker3.innerHTML='change';
 });
 
 
@@ -53,3 +64,19 @@ selectall.addEventListener('change',function(){
     $(this).parents(".box-big").find('input').prop("checked",false);
   }
 }); */
+
+let x1=document.getElementById('x-1');
+let x2=document.getElementById('x-2');
+let aid1=document.getElementById('aid-1');
+let aid2=document.getElementById('aid-2');
+x1.addEventListener('click',function(){
+  aid1.style.display='none';
+});
+x2.addEventListener('click',function(){
+  aid2.style.display='none';
+});
+
+let deletebtn=document.getElementById('delbtn');
+deletebtn.addEventListener('click',function(){
+  
+});
